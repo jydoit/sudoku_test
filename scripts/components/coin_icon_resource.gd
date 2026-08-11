b16402d2571877b3c4a2c0f47a127a085dad7ad8
jiangyu <jiangyu@dailyinnovation.biz>
2026-08-11 08:31:20 +0800

    Include imported coin icon metadata

--- scripts/components/coin_icon_resource.gd
+++ scripts/components/coin_icon_resource.gd
@@ -8,6 +8,10 @@ static var _cached_texture: Texture2D
 static func texture() -> Texture2D:
 	if _cached_texture:
 		return _cached_texture
+	var imported_texture := load(SVG_PATH)
+	if imported_texture is Texture2D:
+		_cached_texture = imported_texture
+		return _cached_texture
 	if not FileAccess.file_exists(SVG_PATH):
 		return null
 	var svg_text := FileAccess.get_file_as_string(SVG_PATH)
